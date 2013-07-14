3a7923c2a59c5ef57e01903ebdd835de00fb80ed
Distributions <tejasdkulkarni@gmail.com>
2013-07-14 19:30:36 -0400

    variational filtering

---
+++ DPMM_Variational/dataset.jl
@@ -0,0 +1,14 @@
+
+function dataset1()
+	theta = {
+	[1/3,2/3,0], 
+	[4/5,1/10,1/10], 
+	[1/8,1/8,6/8]} #3 states
+
+	pi = [1/3,1/3,1/3] 
+	
+	V = 3
+	NUM_TOPICS = 3
+	return theta, pi, NUM_TOPICS, V
+end
+
---
+++ DPMM_Variational/picloud_runner.py
@@ -0,0 +1,44 @@
+
+import sys
+import pickle
+
+import cloud
+cloud.setkey(7513, api_secretkey='ca43a3535fa17e28b687f0f1691c67db261392ae')
+cloud_environment = 'Julia'
+
+"""
+number_of_clusters = int(sys.argv[1])
+if_zero_shortlearning = sys.argv[2] # Should be "yes" or "no"
+experiment_name = sys.argv[3]"""
+
+# Usage: python picloud_runner.py 100 50 10 2
+
+
+TRIALS = int(sys.argv[1])
+NUM_PARTICLES = int(sys.argv[2])
+DELTA = int(sys.argv[3])
+INTEGRAL_PATHS = int(sys.argv[4])
+
+
+def run_on_instance(trial_id):
+  global number_of_clusters
+  global if_zero_shortlearning
+  global experiment_name
+  import subprocess
+  import os
+  os.environ['DISPLAY'] = ":1"
+  print "Starting"
+  ls_output = subprocess.Popen(["/home/picloud/julia/julia", "runner.jl", str(NUM_PARTICLES), str(DELTA), str(INTEGRAL_PATHS)], \
+                               cwd = "/home/picloud/DPMixtureModel/DPMM_SMC/",  \
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+  out, err = ls_output.communicate()
+  return out
+ 
+#result = run_on_instance([1])  
+
+jids = cloud.map(run_on_instance, range(TRIALS), _env=cloud_environment, _type='c2', _cores=1)
+print jids
+result = cloud.result(jids)
+pickle.dump(result, open("result_"+str(NUM_PARTICLES)+"particles_"+str(DELTA)+"delta_"+str(INTEGRAL_PATHS)+"path.pkl","wb"))
+print "RESULT:", result
