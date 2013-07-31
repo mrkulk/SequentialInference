8cc71beb66749451fce8aaa65db60a2302137921
Ardavans <av.saeedi@gmail.com>
2013-07-31 17:24:06 -0400

    added multinomial likelihood

--- IHMM_MaxFilter/maxFilterihmm.jl
+++ IHMM_MaxFilter/maxFilterihmm.jl
@@ -18,6 +18,8 @@ end
 
 TOP_ALPHA = 1
 LOW_ALPHA = 1
+OBS_ALPHA = 1
+NUM_OBS = 4
 SEQUENCE_LENGTH = 10
 
 
@@ -36,20 +38,23 @@ SEQUENCE_LENGTH = 10
 #################GENERATIVE MODEL#################
 
 
-function createCRFProbVect(current_sequence)
+function createCRFProbVect(current_sequence, obs)
 	current_state = current_sequence.current_state
 	dict_of_current_state = current_sequence.suff_stats["low"][current_state]
 	dict_of_state_creation = current_sequence.suff_stats["top"]
+	dict_of_obs_suffstat = current_sequence.suff_stats["obs"]
 	#current_num_states = length(keys(dict_of_current_state)) #only states generated for the current state
 	total_num_states = length(keys(dict_of_state_creation)) #all the states generated
 	total_num_transitions = sum(values(dict_of_current_state))
 	total_num_top_transition = sum(values(dict_of_state_creation))
 
+	denom_obs_part1 = OBS_ALPHA * NUM_OBS
 	prob_vect = zeros(total_num_states * 2 + 1) #one more for the prob of new state creation
 	low_temp_denom = LOW_ALPHA + total_num_transitions
 	for i = 1:total_num_states
 		try
 			prob_vect[i] = dict_of_current_state[i] / low_temp_denom
+			prob_vect[i] *= ((dict_of_obs_suffstat[i][obs] + OBS_ALPHA) / (denom_obs_part1 + sum(values(dict_of_obs_suffstat[i]))) )
 		catch
 			prob_vect[i] = 0
 		end
@@ -57,31 +62,36 @@ function createCRFProbVect(current_sequence)
 
 	top_temp_denom = TOP_ALPHA + total_num_top_transition
 	for i = (total_num_states + 1): (total_num_states * 2)
-		
 		prob_vect[i] = (LOW_ALPHA / low_temp_denom) * dict_of_state_creation[i - total_num_states] / top_temp_denom
+		prob_vect[i] *= ((dict_of_obs_suffstat[i - total_num_states][obs] + OBS_ALPHA) / (denom_obs_part1 + sum(values(dict_of_obs_suffstat[i - total_num_states]))) )
 	end
 
-	prob_vect[length(prob_vect)] = (LOW_ALPHA / low_temp_denom) * (TOP_ALPHA / top_temp_denom)
+	prob_vect[length(prob_vect)] = (LOW_ALPHA / low_temp_denom) * (TOP_ALPHA / top_temp_denom) * (OBS_ALPHA / denom_obs_part1)
 	return prob_vect
 end
 
 
 
 
-function updateIHMMSuffStat(current_sequence, sampled_index, time) #use sampled index to find if it's new or old state 
-	# suff_stats["low"] = Dict()
-	# suff_stats["top"] = Dict()
-	# suff_stats["low"][1] = Dict()
-	# suff_stats["top"][1] = 1
-	# current_state = 1
-	current_state = current_sequence.current_state
+function updateIHMMSuffStat(current_sequence, sampled_index, time, obs) #use sampled index to find if it's new or old state 
+	#sample index is for the 'time' and current_sequence is up to 'time - 1'
+	if time == 1
+		current_state = -1
+	else
+		current_state = current_sequence.current_state
+	end
+
 	dict_of_current_state = current_sequence.suff_stats["low"][current_state]
 	dict_of_state_creation = current_sequence.suff_stats["top"]
+	dict_of_obs_suffstat = current_sequence.suff_stats["obs"]
+
 	total_num_states = length(keys(dict_of_state_creation)) 
 	if sampled_index <= total_num_states
 		dict_of_current_state[sampled_index] += 1
 		current_sequence.seq_history[time] = sampled_index
 		current_sequence.current_state = sampled_index
+		dict_of_obs_suffstat[sampled_index][obs] += 1
+		
 
 	elseif sampled_index > total_num_states & sampled_index <= 2 * total_num_states
 		if contains(dict_of_current_state, sampled_index - total_num_states)
@@ -89,14 +99,24 @@ function updateIHMMSuffStat(current_sequence, sampled_index, time) #use sampled 
 		else
 			dict_of_current_state[sampled_index - total_num_states] = 1
 		end
-		dict_of_state_creation[sampled_index - total_num_states] += 1
+		if contains(dict_of_state_creation, sampled_index - total_num_states)
+			dict_of_state_creation[sampled_index - total_num_states] += 1
+		else
+			dict_of_state_creation[sampled_index - total_num_states] = 1
+		end
 		current_sequence.seq_history[time] = sampled_index - total_num_states
 		current_sequence.current_state = sampled_index - total_num_states
+		dict_of_obs_suffstat[sampled_index - total_num_states][obs] += 1
 	else
 		dict_of_current_state[total_num_states + 1] = 1
 		dict_of_state_creation[total_num_states + 1] = 1
 		current_sequence.seq_history[time] = total_num_states + 1
 		current_sequence.current_state = total_num_states + 1
+		dict_of_obs_suffstat[total_num_states + 1] = Dict()
+		for obs_key = 1:NUM_OBS
+				dict_of_obs_suffstat[total_num_states + 1][obs_key] = 0
+		end
+		dict_of_obs_suffstat[total_num_states + 1][obs] += 1
 	end
 	
 end
@@ -105,28 +125,41 @@ end
 ###################RUNNER ##########################
 
 
+
+
+
+#########INIT SEQ
 suff_stats = Dict()
 suff_stats["low"] = Dict()
 suff_stats["top"] = Dict()
-suff_stats["low"][1] = Dict()
-suff_stats["low"][1][2] = 23
-suff_stats["low"][1][3] = 22
-suff_stats["top"][1] = 1
-suff_stats["top"][2] = 1
-suff_stats["top"][3] = 1
+suff_stats["low"][-1] = Dict()
+#suff_stats["low"][-1][1] = 0
+suff_stats["top"] = Dict()
+#suff_stats["top"][1] = 0
+suff_stats["obs"] = Dict()
+suff_stats["obs"][1] = Dict()
+for obs_key = 1:NUM_OBS
+	suff_stats["obs"][1][obs_key] = 0
+end
+
 seq_history = zeros(SEQUENCE_LENGTH)
-current_sequence = sequence(suff_stats, seq_history, 1)
+current_sequence = sequence(suff_stats, seq_history, -1)
 
-println((createCRFProbVect(current_sequence)))
+println((createCRFProbVect(current_sequence, 1)))
 
-updateIHMMSuffStat(current_sequence, 2, 2)
+updateIHMMSuffStat(current_sequence, 1, 1, 1)
 println(current_sequence)
 
 
 
 
 
-
+########################################################################
+# suff_stats["low"][1][2] = 23
+# suff_stats["low"][1][3] = 22
+# suff_stats["top"][1] = 1
+# suff_stats["top"][2] = 1
+# suff_stats["top"][3] = 1
 
 
 
