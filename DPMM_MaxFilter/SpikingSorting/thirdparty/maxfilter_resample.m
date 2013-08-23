9194e79f37ad1fd0e19a22e162b7097bed0883bd
Distributions <tejasdkulkarni@gmail.com>
2013-08-23 02:41:59 -0400

    added spike sorting prelim exp

---
+++ DPMM_MaxFilter/SpikingSorting/thirdparty/maxfilter_resample.m
@@ -0,0 +1,57 @@
+function [rx] = maxfilter_resample(x,w,N)
+% function [rx] = multinomial_resample(x,w,N)
+%
+%  returns N samples from the weighted particle set 
+%  x,w with x (DxM) being M, D-dimensional samples and w being a vector of
+%  M real weights that sum to one
+%
+[D, M] = size(x);
+rx = zeros(D,N);
+rw = zeros(1,N);
+
+[sorted_w, sorted_indx_array] = sort(w, 'descend');
+
+hash = containers.Map;
+cnt=1;
+record_indx = [];
+
+for i=1:length(sorted_w)
+    sampled_ind = sorted_indx_array(i); 
+    if isKey(hash, int2str(x(:,sampled_ind))) == 0
+        hash(int2str(x(:,sampled_ind))) = 1;
+        record_indx(cnt)=sampled_ind;
+        cnt=cnt+1;
+    end
+end
+
+
+len_rec = length(record_indx);
+cnt = 1;
+for i = 1:N
+    if i <= len_rec
+        sampled_ind = record_indx(i);
+    else
+        sampled_ind = sorted_indx_array(cnt); 
+        cnt=cnt+1;
+    end
+    rx(:,i) = x(:,sampled_ind);
+    rw(i) = w(sampled_ind);
+end
+
+
+% 
+% len_rind = length(record_indx);
+% pind=1;
+% for i = 1:N
+%     if pind > len_rind
+%         pind=1;
+%     end
+%     sampled_ind = record_indx(pind);
+%     rx(:,i) = x(:,sampled_ind);
+%     rw(i) = w(sampled_ind);
+%     pind=pind+1;
+% end
+
+
+rw = rw./sum(rw);
+    
